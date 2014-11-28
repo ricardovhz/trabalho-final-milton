8d47c21e0a3d702decb028df91390be329f4cb65
ricardovhz <ricardo.vanhoz@gmail.com>
2014-11-28 16:57:43 -0200

    commit

--- src/br/com/vanhoz/ricardo/trabalho/milton/main/RedeNeuralExecute.java
+++ src/br/com/vanhoz/ricardo/trabalho/milton/main/RedeNeuralExecute.java
@@ -20,65 +20,76 @@ public class RedeNeuralExecute {
 		rede.loadPesos(pesos);
 	}
 	
-	public double getIdFromCapture() {
-		try {
-//			final String PATH="/tmp/audio.wav";
-			AudioRecorder ar = new AudioRecorder();
-//			ar.capture(2000);
-//			ar.saveDataToWAV(PATH);
-			ar.captureFromFile("ricardo-out.wav"); 
-			
-			SoundAnalyzer analyzer = new SoundAnalyzer(ar.getCapturedAudio());
-			List<AnalysisResult> results = analyzer.getFirstBiggestResults(5);
-			
-			double[] ids = new double[5];
-			
-			for (int i=0;i<results.size();i++) {
-				AnalysisResult result = results.get(i);
-				System.out.println(result);
-				double[] freq = Utils.getBits(result.getFrequency(), 11);
-				double[] val = Utils.getBits(result.getValue(), 17);
-				double[] pos = Utils.getBits(i, 3);
-				
-				for (int j=0;j<freq.length;j++) {
-					rede.setValoresEntradas(j, new EntradaSimples(freq[j]));
-				}
+	public int getIdFromMicrofone() throws IOException {
+		final String PATH="/tmp/audio.wav";
+		AudioRecorder ar = new AudioRecorder();
+		ar.capture(2000);
+		ar.saveDataToWAV(PATH);
+		return getIdFromCapture(ar.getCapturedAudio());
+	}
 
-				for (int j=0;j<val.length;j++) {
-					rede.setValoresEntradas(11+j, new EntradaSimples(val[j]));
-				}
+	public int getIdFromCaptureFile(String file) throws IOException {
+		AudioRecorder ar = new AudioRecorder();
+		ar.captureFromFile(file);
+		return getIdFromCapture(ar.getCapturedAudio());
+	}
 
-				for (int j=0;j<pos.length;j++) {
-					rede.setValoresEntradas(28+j, new EntradaSimples(pos[j]));
-				}
-				
-				Linha l = rede.getResult();
-				
-				double id = 0.0D;
-				for (int j=0;j<l.getNeuronios().size();j++) {
-					double d = l.getNeuronios().get(j).saida();
-					System.out.println(d);
-					id += (l.getNeuronios().size()-j) * new BigDecimal(d).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-				}
-				
-				ids[i] = id;
+	private int getIdFromCapture(byte[] data) {
+		SoundAnalyzer analyzer = new SoundAnalyzer(data);
+		List<AnalysisResult> results = analyzer.getFirstBiggestResults(5);
+		
+		double[] ids = new double[5];
+		
+		for (int i=0;i<results.size();i++) {
+			AnalysisResult result = results.get(i);
+			System.out.println(result);
+			double[] freq = Utils.getBits(result.getFrequency(), 11);
+			double[] val = Utils.getBits(result.getValue(), 17);
+			double[] pos = Utils.getBits(i, 3);
+			
+			for (int j=0;j<freq.length;j++) {
+				rede.setValoresEntradas(j, new EntradaSimples(freq[j]));
+			}
+
+			for (int j=0;j<val.length;j++) {
+				rede.setValoresEntradas(11+j, new EntradaSimples(val[j]));
+			}
+
+			for (int j=0;j<pos.length;j++) {
+				rede.setValoresEntradas(28+j, new EntradaSimples(pos[j]));
 			}
 			
-			double media = 0.0D;
-			for (int i=0;i<ids.length;i++) {
-				media += ids[i];
+			Linha l = rede.getResult();
+			
+			double id = 0.0D;
+			for (int j=l.getNeuronios().size()-1;j>=0;j--) {
+				double d = l.getNeuronios().get(j).saida();
+				System.out.println(d);
+//					System.out.println(Math.pow(2, j));
+//					System.out.println(new BigDecimal(d).setScale(1, BigDecimal.ROUND_HALF_DOWN));
+				id += Math.pow(2, j) * (new BigDecimal(d).setScale(0, BigDecimal.ROUND_HALF_UP).doubleValue());
+//					id += Math.pow(2, j) * d;
 			}
-			media /= ids.length;
-			return media;
-		} catch (IOException e) {
-			e.printStackTrace();
+			
+			ids[i] = id;
 		}
-		throw new RuntimeException("Um erro ocorreu");
+		
+		double media = 0.0D;
+		for (int i=0;i<ids.length;i++) {
+			media += ids[i];
+		}
+		media /= ids.length;
+		return (int)media;
 	}
 	
 	public static void main(String[] args) {
-		RedeNeuralExecute r = new RedeNeuralExecute();
-		double id = r.getIdFromCapture();
-		System.out.println("\n============\n"+id);
+		try {
+			RedeNeuralExecute r = new RedeNeuralExecute();
+			int id = r.getIdFromCaptureFile("hi.wav");
+			System.out.println("\n============\n"+id);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
